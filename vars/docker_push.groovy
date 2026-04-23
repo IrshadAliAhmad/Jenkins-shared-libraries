5a303999ecf19cfa10e739a7adb2385244c2a4ea
Irshad Ahmad <150771186+IrshadAliAhmad@users.noreply.github.com>
2026-04-23 10:03:04 +0530

    Uncomment Docker login and push commands

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -4,13 +4,12 @@ def call(String project, String imageTag, String dockerHubUser){
                     usernameVariable: 'DOCKER_USER',
                     passwordVariable: 'DOCKER_PASS'
                 )]){
-                   // sh '''
-                   // echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
-                   // docker image tag todo-img:latest $DOCKER_USER/todo-img:latest
-                   // docker push $DOCKER_USER/todo-img:latest
-                   // '''
+                   sh '''
+                     echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+                     docker push $DOCKER_USER/todo-img:latest
+                    '''
                   // ya
-                 sh "docker login -u $(DOCKER_USER) -p $(DOCKER_PASS)
+                 // sh "docker login -u $(DOCKER_USER) -p $(DOCKER_PASS)
                 }
-                 sh "docker push ${dockerHubUser}/${project}:${imageTag}"
+                 // sh "docker push ${dockerHubUser}/${project}:${imageTag}"
 }
